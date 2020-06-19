19da753f3ccbe7686af27f5da23225d1425c491e
sankar <sankarpn@gmail.com>
2020-06-19 07:53:45 -0700

    wip

--- new-integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/BuildApplication.java
+++ new-integration-tests/src/test/java/oracle/weblogic/kubernetes/utils/BuildApplication.java
@@ -122,14 +122,18 @@ public class BuildApplication {
     String pvName = namespace + "-build-pv-" + uniqueName;
     String pvcName = namespace + "-build-pvc-" + uniqueName;
 
-    Path pvPath = Paths.get(PV_ROOT, "j2eeapplications", application.getFileName().toString());
-    createPV(pvPath, pvName);
+    Path pvHostPath = Paths.get(PV_ROOT, "j2eeapplications", application.getFileName().toString());
+    createPV(pvHostPath, pvName);
     createPVC(pvName, pvcName, namespace);
 
     V1Pod webLogicPod = setupWebLogicPod(namespace, pvcName, pvName);
     Kubernetes.copyFileToPod(namespace, webLogicPod.getMetadata().getName(),
         null, zipFile, Paths.get(APPLICATIONS_MOUNT_PATH, zipFile.getFileName().toString()));
-    Kubernetes.exec(webLogicPod, new String[]{"sh", "-c", "ls /application"});
+    Kubernetes.exec(webLogicPod, new String[]{
+      "sh", "-c", "ls /application;",
+      "cd /application;",
+      "unzip " + zipFile.getFileName() + ";",
+       "ls "});
     /*
     ExecResult exec = Exec.exec(webLogicPod, null, true,
         "sh -c ls /;"
